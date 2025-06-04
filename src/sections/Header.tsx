8cc9bf40639f8c5957ae629582999d28d2b67caa
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-04 14:30:10 +0530

    feat(Header): enhance navigation with animated dropdown and improve accessibility

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,7 +3,6 @@ import Button from "@/components/Button";
 import { useEffect, useState } from "react";
 import { motion, useAnimate } from "motion/react";
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const navItems = [
   {
     label: "About",
@@ -29,39 +28,89 @@ const navItems = [
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [topLineScope, setTopLineScope] = useAnimate();
-  const [bottomLineScope, setBottomLineScope] = useAnimate();
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+  const [navScope, navAnimate] = useAnimate();
 
   useEffect(() => {
     if (isOpen) {
-      setTopLineScope([
+      topLineAnimate([
         [topLineScope.current, { translateY: 4 }],
         [topLineScope.current, { rotate: 45 }],
       ]);
-      setBottomLineScope([
+      bottomLineAnimate([
         [bottomLineScope.current, { translateY: -4 }],
         [bottomLineScope.current, { rotate: -45 }],
       ]);
+      navAnimate(
+        navScope.current,
+        {
+          height: "100%",
+        },
+        {
+          duration: 0.7,
+        }
+      );
     } else {
-      setTopLineScope([
+      topLineAnimate([
         [topLineScope.current, { rotate: 0 }],
         [topLineScope.current, { translateY: 0 }],
       ]);
-      setBottomLineScope([
+      bottomLineAnimate([
         [bottomLineScope.current, { rotate: 0 }],
         [bottomLineScope.current, { translateY: 0 }],
       ]);
+      navAnimate(navScope.current, {
+        height: 0,
+      });
     }
   }, [
     isOpen,
-    setTopLineScope,
-    setBottomLineScope,
+    topLineAnimate,
+    bottomLineAnimate,
     bottomLineScope,
     topLineScope,
+    navScope,
+    navAnimate,
   ]);
 
   return (
     <header className="">
+      <div
+        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900"
+        ref={navScope}
+      >
+        <nav className="mt-20 flex flex-col">
+          {navItems.map(({ label, href }) => (
+            <a
+              key={label}
+              href={href}
+              className="text-stone-200 border-t border-stone-800 last:border-b py-8 group/nav-item relative"
+            >
+              <div className="p-[1rem] md:p-[2rem] lg:p-[2.6rem] !max-w-full flex items-center justify-between">
+                <span className="text-3xl group-hover/nav-item:pl-4 transition-all duration-500">
+                  {label}
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                  />
+                </svg>
+              </div>
+              <div className="absolute w-full h-0 bg-stone-800 group-hover/nav-item:h-full transition duration-500"></div>
+            </a>
+          ))}
+        </nav>
+      </div>
       <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md ">
         <div className="flex items-center justify-between h-20 !max-w-full p-[1rem] md:p-[2rem] lg:p-[2.6rem]">
           <div className="text-xl md:text-2xl flex items-center font-medium uppercase tracking-tight text-white">
