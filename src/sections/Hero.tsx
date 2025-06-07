9b4dc3f8b75bc8c449f7843fd8e4fabbd5a5499a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-07 06:27:33 +0530

    feat(Hero): integrate SplitType for animated text and enhance motion effects

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,19 +1,43 @@
-import { FC } from "react";
-
+"use client";
+import { FC, useEffect } from "react";
 import heroImage from "@/assets/images/hero-image.jpg";
 import Button from "@/components/Button";
 import Image from "next/image";
+import SplitType from "split-type";
+import { useAnimate, motion, stagger } from "motion/react";
 
 const Hero: FC = () => {
+  const [titleScope, titleAnimate] = useAnimate();
+  useEffect(() => {
+    new SplitType(titleScope.current, {
+      types: "lines,words",
+      tagName: "span",
+    });
+    titleAnimate(
+      titleScope.current.querySelectorAll(".word"),
+      {
+        transform: "translateY(0)",
+      },
+      {
+        duration: 0.5,
+        delay: stagger(0.2),
+      }
+    );
+  }, []);
   return (
     <>
       <section>
         <div className="grid md:grid-cols-12 md:h-screen items-stretch">
           <div className="md:col-span-7 flex flex-col justify-center">
             <div className="!max-w-full p-[1rem] md:p-[2rem] lg:p-[4rem]">
-              <h1 className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0"
+                ref={titleScope}
+              >
                 Crafting digital experience through code and creative design
-              </h1>
+              </motion.h1>
               <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
                 <Button
                   variant="secondary"
