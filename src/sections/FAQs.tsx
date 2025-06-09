f45d3422823c477b2f657c06b810fa74dbc39f7e
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-09 15:26:23 +0530

    feat(FAQs): enhance FAQ section with animated reveal and improved interaction

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -1,4 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
 const faqs = [
   {
     question: "How long does it take to build a website?",
@@ -23,21 +27,41 @@ const faqs = [
 ];
 
 const FAQs = () => {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   return (
     <section className="py-24 md:py-32 lg:py-40" id="faqs">
       <div className="p-[1rem] md:p-[2rem] lg:p-[2.6rem]">
         <h2 className="text-4xl md:text-7xl lg:text-8xl">FAQs</h2>
         <div className="mt-10 md:mt-16 lg:mt-20">
-          {faqs.map(({ question, answer }) => (
+          {faqs.map(({ question, answer }, faqIndex) => (
             <div
               key={question}
-              className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10 last:border-b"
+              className="border-t border-stone-400 border-dotted py-6 md:py-8 lg:py-10 last:border-b relative isolate group/faq"
+              onClick={() =>
+                setSelectedIndex(faqIndex === selectedIndex ? null : faqIndex)
+              }
             >
-              <div className="flex items-center justify-between gap-4">
+              <div
+                className={twMerge(
+                  "absolute h-0 w-full bg-stone-300 bottom-0 left-0 -z-10 group-hover/faq:h-full transition-all duration-700",
+                  faqIndex === selectedIndex && "h-full"
+                )}
+              ></div>
+              <div
+                className={twMerge(
+                  "flex items-center justify-between gap-4 transition-all duration-700 group-hover/faq:lg:px-8",
+                  faqIndex === selectedIndex && "lg:px-8"
+                )}
+              >
                 <div className="text-2xl md:text-3xl lg:text-4xl">
                   {question}
                 </div>
-                <div className="inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0">
+                <div
+                  className={twMerge(
+                    "inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0 transition duration-300 bg-stone-200",
+                    faqIndex === selectedIndex && "rotate-45"
+                  )}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -54,6 +78,21 @@ const FAQs = () => {
                   </svg>
                 </div>
               </div>
+              <AnimatePresence>
+                {selectedIndex === faqIndex && (
+                  <motion.div
+                    className="overflow-hidden lg:px-8"
+                    initial={{ height: 0 }}
+                    animate={{ height: "auto" }}
+                    exit={{ height: 0 }}
+                    transition={{ duration: 0.7, ease: "easeInOut" }}
+                  >
+                    <p className="mt-4 md:mt-6 lg:mt-8 text-lg md:text-xl lg:text-2xl">
+                      {answer}
+                    </p>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
           ))}
         </div>
