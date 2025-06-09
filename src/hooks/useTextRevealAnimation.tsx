57b2c400e16a1901dbe4fceb24b14589ad619552
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-09 13:29:37 +0530

    feat(TextReveal): add custom hook for text reveal animation using SplitType and motion

---
+++ src/hooks/useTextRevealAnimation.tsx
@@ -0,0 +1,30 @@
+import { stagger, useAnimate } from "motion/react";
+import { useEffect } from "react";
+import SplitType from "split-type";
+
+const useTextRevealAnimation = () => {
+  const [scope, animate] = useAnimate();
+  useEffect(() => {
+    new SplitType(scope.current, {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }, [scope]);
+
+  const entranceAnimation = () => {
+    return animate(
+      scope.current.querySelectorAll(".word"),
+      {
+        transform: "translateY(0)",
+      },
+      {
+        duration: 0.5,
+        delay: stagger(0.2),
+      }
+    );
+  };
+
+  return { scope, entranceAnimation };
+};
+
+export default useTextRevealAnimation;
