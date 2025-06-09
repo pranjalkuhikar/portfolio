0e45d9cf43722f10eaa79c4c2b6b8b401bfe1013
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-09 15:43:44 +0530

    feat(Footer): refactor navigation items and enhance animations with text reveal effects

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,59 +1,105 @@
+"use client";
+
 import Button from "@/components/Button";
+import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
+import { useInView } from "motion/react";
+import { useEffect } from "react";
+
+const navItems = [
+  {
+    href: "#intro",
+    label: "About",
+  },
+  {
+    href: "#projects",
+    label: "Projects",
+  },
+  {
+    href: "#testimonials",
+    label: "Testimonials",
+  },
+  {
+    href: "#faqs",
+    label: "Faqs",
+  },
+  {
+    href: "#contact",
+    label: "Contact",
+  },
+];
 
 const Footer = () => {
-  const navItems = [
-    {
-      href: "#",
-      label: "Home",
-    },
-    {
-      href: "#",
-      label: "Projects",
-    },
-    {
-      href: "#",
-      label: "Testimonials",
-    },
-    {
-      href: "#",
-      label: "Faqs",
-    },
-    {
-      href: "#",
-      label: "Contact",
-    },
-  ];
+  const { scope, entranceAnimation } = useTextRevealAnimation();
+  const inView = useInView(scope);
+
+  useEffect(() => {
+    if (inView) {
+      entranceAnimation();
+    }
+  }, [inView, entranceAnimation]);
+
+  const handleClickNavItem = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/, "");
+    const element = document.getElementById(targetId);
+    element?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   return (
     <footer className="bg-stone-900 text-white" id="contact">
       <div className="p-[1rem] md:p-[2rem] lg:p-[2.6rem]">
         <div className="py-24 md:py-32 lg:py-40">
           <div className="flex items-center gap-3">
-            <div className="size-3 rounded-full bg-green-400"></div>
+            <div className="size-3 rounded-full bg-green-400 animate-pulse"></div>
             <span className="uppercase">One spot available for next month</span>
           </div>
           <div className="grid md:grid-cols-3 md:items-center">
             <div className="md:col-span-2">
-              <h2 className="text-4xl md:text-7xl mt-8 lg:text-8xl font-extralight">
+              <h2
+                className="text-4xl md:text-7xl mt-8 lg:text-8xl font-extralight"
+                ref={scope}
+              >
                 Enough talk, Let&apos;s make something great together.
               </h2>
               <Button
                 variant="secondary"
                 className="mt-8"
                 iconAfter={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                  <div className="size-6 overflow-hidden">
+                    <div className="w-12 h-6 flex transition-transform duration-500 group-hover/button:-translate-x-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 }
               >
                 info@pranjalkuhikar.com
@@ -62,7 +108,7 @@ const Footer = () => {
             <div>
               <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
                 {navItems.map(({ href, label }) => (
-                  <a href={href} key={label}>
+                  <a href={href} key={label} onClick={handleClickNavItem}>
                     <Button variant="text" className="text-lg">
                       {label}
                     </Button>
