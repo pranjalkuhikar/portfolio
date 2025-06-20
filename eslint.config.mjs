be85bea733298298a289b3fcc266a100cc6d1ae1
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-20 11:22:53 +0530

    chore: disable unused variable rule for TypeScript in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  },
 });
 
 const eslintConfig = [
